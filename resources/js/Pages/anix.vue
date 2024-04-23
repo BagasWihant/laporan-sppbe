3b48493584aa190a21ff287680f48ca667eb4f9c
BagasWihant <bagaswihant@gmail.com>
2024-04-23 15:58:19 +0700

    setup daisyui

--- resources/js/Pages/anix.vue
+++ resources/js/Pages/anix.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <h1>HAII</h1>
+        <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-outline">Default</button>
+<button class="btn btn-outline btn-primary">Primary</button>
+<button class="btn btn-outline btn-secondary">Secondary</button>
+<button class="btn btn-outline btn-accent">Accent</button>
     </div>
 </template>
 
