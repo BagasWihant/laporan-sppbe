e1506384393b153a1cf972bac128161dfa76887d
BagasWihant <bagaswihant@gmail.com>
2024-05-08 16:20:45 +0700

    new: sub menu

--- resources/js/Components/SidebarMenu.vue
+++ resources/js/Components/SidebarMenu.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="rounded-box gap-2">
-        <template v-for="menu in menuGroups" :key="menu.id">
-            <h3>{{ menu.name }}</h3>
+  <div class="rounded-box gap-2">
+    <template v-for="menu in menuGroups" :key="menu.id">
+      <h3>{{ menu.name }}</h3>
 
-            <ul>
-                <SidebarMeniItem v-for="(menuItem,index) in menu.menuItems"
-                :item="menuItem" :key="index" :index="index" ></SidebarMeniItem>
-            </ul>
-        </template>
-    </div>
+      <ul>
+        <SidebarMeniItem v-for="(menuItem, index) in menu.menuItems" :item="menuItem" :key="index" :index="index">
+        </SidebarMeniItem>
+      </ul>
+    </template>
+  </div>
 </template>
 
 <script setup>
@@ -20,7 +20,7 @@ const menuGroups = ref([
   {
     name: 'MENU',
     menuItems: [
-     
+
       {
         icon: `<svg
                   class="fill-current"
@@ -39,7 +39,7 @@ const menuGroups = ref([
         route: 'dashboard',
         rt: 'dashboard',
       },
-      
+
       {
         icon: `<svg
                   class="fill-current"
@@ -74,11 +74,36 @@ const menuGroups = ref([
         rt: 'master',
         route: '#',
         child: [
-          { label: 'Data Barang', route: '/barang',rt:'barang' },
-          { label: 'Data Perusahaan', route: '/perusahaan',rt:'perusahaan' }
+          { label: 'Data Barang', route: '/barang', rt: 'barang' },
+          { label: 'Data Perusahaan', route: '/perusahaan', rt: 'perusahaan' }
         ]
       },
-      
+
+      {
+        icon: `<svg viewBox="0 0 512 512"   width="18"
+                  height="18" xmlns="http://www.w3.org/2000/svg"><path d="M152 256h144C309.3 256 320 245.3 320 232S309.3 208 296 208h-144C138.8 208 128 218.8 128 232S138.8 256 152 256zM152 160h208C373.3 160 384 149.3 384 136S373.3 112 360 112h-208C138.8 112 128 122.8 128 136S138.8 160 152 160zM152 352h272c13.25 0 24-10.75 24-24S437.3 304 424 304h-272C138.8 304 128 314.8 128 328S138.8 352 152 352zM488 432H48V56C48 42.75 37.25 32 24 32S0 42.75 0 56V448c0 17.59 14.41 32 32 32h456c13.25 0 24-10.75 24-24S501.3 432 488 432z"/></svg>`,
+        label: 'Laporan',
+        rt: 'laporan',
+        route: '#',
+        child:[
+          {
+            label: 'RAB',
+            route: '/laporan/rab',
+            rt: 'rab'
+          },
+          {
+            label: 'Purchase Order',
+            route: '/laporan/po',
+            rt: 'po'
+          },
+          {
+            label: 'Invois',
+            route: '/laporan/invois',
+            rt: 'invois'
+          },
+        ]     
+      }
+
     ]
   },
   {
