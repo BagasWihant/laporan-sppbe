5381fe12bd96f42f04d8cebe86c45e919421fcb0
bagaswihant <bagaswihant@gmail.com>
2024-05-11 21:49:43 +0700

    fix: sidebar 2

--- resources/js/stores/sidebar.js
+++ resources/js/stores/sidebar.js
@@ -6,10 +6,11 @@ export const useSidebarStore = defineStore('sidebar', () => {
   const isSidebarOpen = ref(false)
   const selected = useStorage('selected', ref('eCommerce'))
   const page = useStorage('page', ref('Dashboard'))
+  const click = useStorage('click', ref('dashboard'))
 
   function toggleSidebar() {
     isSidebarOpen.value = !isSidebarOpen.value
   }
 
-  return { isSidebarOpen, toggleSidebar, selected, page }
+  return { isSidebarOpen, toggleSidebar, selected, page ,click}
 })
